c2f6cb339cede9688820eeb4f55a4542645e8464
wz2cool <wz2cool@live.cn>
2018-09-27 16:27:58 +0800

    add

--- src/Converter.tsx
+++ src/Converter.tsx
@@ -1,25 +1,38 @@
 import * as React from "react";
-import { Row, Col, Input, Layout } from "antd";
-const { Header, Content } = Layout;
+import { Button, Input, Tabs } from "antd";
 
 const { TextArea } = Input;
+const TabPane = Tabs.TabPane;
+
+import "./Converter.scss";
 
 class Converter extends React.Component {
   render() {
     return (
-      <Row style={{ height: "100%" }}>
-        <Col span={12} style={{ height: "100%" }}>
-          <Layout style={{ height: "100%" }}>
-            <Header />
-            <Content>
-              <TextArea style={{ height: "100%" }} />
-            </Content>
-          </Layout>
-        </Col>
-        <Col span={12} style={{ height: "100%" }}>
-          col-12
-        </Col>
-      </Row>
+      <div className="converter">
+        <div className="left-area">
+          <div className="header">
+            <Button type="primary">Convert</Button>
+          </div>
+          <div className="content">
+            <TextArea className="input-query" />
+          </div>
+        </div>
+        <div className="right-area">
+          <Tabs defaultActiveKey="1">
+            <TabPane tab="Tab 1" key="1">
+              Content of Tab Pane 1
+            </TabPane>
+            <TabPane tab="Tab 2" key="2">
+              Content of Tab Pane 2
+            </TabPane>
+            <TabPane tab="Tab 3" key="3">
+              Content of Tab Pane 3
+            </TabPane>
+          </Tabs>
+          ,
+        </div>
+      </div>
     );
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,14 @@ module.exports = {
         use: ["style-loader", "css-loader"]
       },
       {
+        test: /\.scss$/,
+        use: [
+          "style-loader", // creates style nodes from JS strings
+          "css-loader", // translates CSS into CommonJS
+          "sass-loader" // compiles Sass to CSS, using Node Sass by default
+        ]
+      },
+      {
         test: /\.tsx?$/,
         use: ["awesome-typescript-loader"]
       }
